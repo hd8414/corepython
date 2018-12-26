452a96d8043d3902aaf8f052c2565b8020fe82f3
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2018-12-26 22:54:27 +0000

    hi

--- Basic math.py
+++ Basic math.py
@@ -1,8 +1,16 @@
-a = 10
-b = 3
-print ('sum = ',a+b)
-print ('sub = ', a-b)
-print ('multi = ',a*b)
-print ('div = ',a/b)
-print ('mod = ',a%b)
-print ('power = ',a**b)
+# program to do simple maths of two value
+
+a = float(input('enter value of a :'))
+b = float(input('enter value of b :'))
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+h = a**b
+print ('sum of a and b = %.f'%c)
+print ('sub of a and b = %.2f'%d)
+print ('multi of a and b = %.3f'%e)
+print ('div of a by b = %.1f'%f)
+print ('mod of a by a = %.2f'%g)
+print ('power of a of b = %.2f'%h)
