71bdc3cc55268cb73274fd2724c67d7368e2d6df
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2018-12-26 23:11:49 +0000

    hi

---
+++ Basic maths without extra variable.py
@@ -0,0 +1,11 @@
+# program to do basic maths without adding extra variable
+
+a = int(input('Enter value of a :'))
+b = int(input('Enter value of b :'))
+
+print ('%d + %d = %.1f'%(a,b,a+b))
+print ('%d - %d = %.2f'%(a,b,a-b))
+print ('%d * %d = %.3f'%(a,b,a*b))
+print ('%d / %d = %.4f'%(a,b,a/b))
+print ('%d %% %d = %.5f'%(a,b,a%b))
+print ('%d ** %d = %.6f'%(a,b,a**b))
