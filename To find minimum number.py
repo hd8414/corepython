20befb18fdad4d5924aaf4b73aaec76fad131fed
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2018-12-28 21:38:23 +0000

    hi

--- To find minimum number.py
+++ To find minimum number.py
@@ -4,7 +4,16 @@ number_1 = float(input('Enter 1st number :'))
 number_2 = float(input('Enter 2nd number :'))
 number_3 = float(input('Enter 3rd number :'))
 
-if number_1 < number_2:
+
+if number_1 == number_2 and number_1 != number_3 and number_2 != number_3:
+    print('input is incorrect as 1st and 2nd numbers are same with value %d'%number_1)
+elif number_2 == number_3 and number_2 != number_1 and number_1 != number_3:
+    print('input is incorrect as 2nd and 3rd numbers are same with value %d'%number_2)
+elif number_1 == number_3 and number_1 != number_2 and number_2 != number_3:
+    print('input is incorrect as 1st and 3rd numbers are same with value %d'%number_1)
+elif number_1 == number_2 == number_3 :
+    print('input incorrect as entered value %d of all three numbers are same'%number_1)
+elif number_1 < number_2:
     if number_1 < number_3:
         print('1st number is minimum with value %.2f out of three number'%number_1)
     else:
