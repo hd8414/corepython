5456fef4d2e0b03d74b5ac90dd723ca818384913
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2018-12-29 19:41:22 +0000

    hi

---
+++ Basic loop.py
@@ -0,0 +1,37 @@
+# print 0 to 9 number
+for i in range(10):
+    print (i,end =' ')
+print (' ') # add print to add next look optput in new line
+
+
+# print number from 5 to 9 in range from 0 to 9
+print (' ') # add print to space 1 line
+for i in range(5,10):
+    print (i, end=' ')
+print (' ')
+
+# print odd numbers in range from 0 to 49
+print (' ')
+for i in range(1,50,2):
+    print (i,end=' ')
+print (' ')
+
+# print even numbers in range from 0 to 49
+print (' ')
+for i in range(0,50,2):
+    print (i,end=' ')
+print (' ')
+
+# print number 0,5,,,45, from range of 0 to 49
+print ('')
+for i in range(0,50,5):
+    print (i,end=' ')
+print (' ')
+
+# print name 10 times
+print (' ')
+for hardik in range(10):
+    print('hardik', end=' ')
+print (' ')
+
+
