fd1f3b597b8bd3e13a86f589355c11353d3b6e8a
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2019-01-02 21:17:55 +0000

    hi

--- Loop for pattern F.py
+++ Loop for pattern F.py
@@ -3,6 +3,6 @@ for row in range (7):
         if row==0 or row==3 or col==0:
             print('*',end=' ')
         else:
-            print(end='  ')
+            print(end='   ')
     print('')
 print('')
