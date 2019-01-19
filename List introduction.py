fd8f57ec514d2f7aaf3a31d6750e2f32bcb2cd96
HARDIK PATEL <44933263+hd8414@users.noreply.github.com>
2019-01-19 00:27:25 +0000

    hi

---
+++ List introduction.py
@@ -0,0 +1,89 @@
+# empty list
+animals = [ ]
+print (animals)
+
+# list with items
+animals1 = ['dog', 'cat', 'monkey']
+print (animals1)
+
+# indexing in list
+print (animals1[0])
+
+# list with mix type of elements
+list = [10, 10.5, 'hardik', 25.52]
+print (list)
+
+# nesting in list
+list2 = [[10,20],[30,40],[50,60]]
+print (list2[2])
+print (list2[1][0]) # 1st element on second list
+
+# example of list
+
+for num in [100,200,300,400]:
+    print (num)
+
+print ( )
+
+'''************************************************************************'''
+
+''' LIST OPERATION '''
+# REPLACE
+list3 = [5,15,25,35]
+list3[2] = 3.7
+print(list3)
+print ()
+
+# INSERT
+list3.insert(2,'hello')
+print(list3)
+print ()
+
+# SORT - LIST HAS TO HAVE SAME ELEMENTS
+animal = ['dog','cat','monkey']
+animal.sort()
+print(animal)
+list4 = [49,29,9,39,19]
+list4.sort()
+print(list4)
+
+# delete element
+del list4[2] # deleting single element
+print (list4)
+del list4 # deleting full list
+
+# APPEND
+animal.append('donkey')
+print (animal)
+
+# REVERSE
+animal.reverse()
+print(animal)
+
+# example 2
+fruits = ['apple','banana']
+print(fruits)
+
+fruits[1] = 'grapes' # replace
+print (fruits)
+
+fruits.insert(1,'orange')   # insert
+print(fruits)
+
+fruits.sort()
+print(fruits)               # sort
+
+del fruits[0]
+print (fruits)              # delete
+
+fruits.append('cherry')
+print(fruits)               # append
+
+fruits.reverse()            # reverse
+
+fruits.clear()
+print (fruits)               #clear
+
+
+
+
